69476bda0afde40c555785db9ebb0d5eb6bda442
Daniel Silveira <danieldasilvasilveira2002@gmail.com>
2024-10-19 12:33:50 -0300

    Desafio completo

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,21 +4,27 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
-        public void Ligar()
+        public void Ligar(string numeroLigando)
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine("Ligando para: " + numeroLigando);
         }
 
-        public void ReceberLigacao()
+        public void ReceberLigacao(string numeroRecebendo)
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine("Recebendo ligação de: " + numeroRecebendo);
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
+Smartphone nokia = new Nokia(numero: "987654321", modelo: "10", imei: "192837465", memoria: 32);
+Smartphone iphone = new Iphone(numero: "123456789", modelo: "5", imei: "918273645", memoria: 64);
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+nokia.ReceberLigacao(numeroRecebendo: iphone.Numero);
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+Console.WriteLine("---------------------------------");
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone IPhone: ");
+iphone.Ligar(numeroLigando: nokia.Numero);
+iphone.InstalarAplicativo("Whatsapp");
